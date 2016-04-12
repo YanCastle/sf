441a2ecd993e8d316a86ec0e63a9fd2efbb92cb7
鄢鹏权 <castle@tansuyun.cn>
2016-04-12 11:37:35 +0800

    [+]添加Swoole的HTTP类型和Socket类型

---
+++ Tsy/Library/Swoole/Http.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Castle
+ * Date: 2016/4/12
+ * Time: 11:36
+ */
+
+namespace Tsy\Library\Swoole;
+
+
+use Tsy\Library\Swoole;
+
+class Http extends Swoole
+{
+
+}
