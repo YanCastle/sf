dc72ff7486c68e790b6414649e53ee79d4fc4e51
鄢鹏权 <castle@tansuyun.cn>
2016-04-13 15:15:53 +0800

    [#]添加空控制器和空方法

--- TsyExample/Example/Application/Controller/IndexController.php
+++ TsyExample/Example/Application/Controller/IndexController.php
@@ -13,5 +13,12 @@ use Tsy\Library\Controller;
 
 class IndexController extends Controller
 {
+    function index(){}
 
+    /**
+     * 空操作
+     * @param string $Action 方法名称
+     * @param array|string $Data 数据
+     */
+    function _empty($Action,$Data){}
 }
