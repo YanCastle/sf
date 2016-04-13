94a5401761bd8862d15834dfabea4b0c50645bf0
鄢鹏权 <castle@tansuyun.cn>
2016-04-13 16:56:51 +0800

    [#]Controller基类结构定义

--- Tsy/Library/Controller.class.php
+++ Tsy/Library/Controller.class.php
@@ -11,5 +11,14 @@ namespace Tsy\Library;
 
 class Controller
 {
-
+    protected $swoole;
+    function __construct()
+    {
+    }
+    function set_swoole($swoole){
+        $this->swoole=$swoole;
+    }
+    protected function send($UID,$data){
+        //TODO 需要建立UID跟fd的连接信息，如果不是在swoole模式下还需要放到队列中去
+    }
 }
