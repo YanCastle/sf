31b5e230c14a93b5d887c85b321ad374de9db2b5
龙虾 <326956600@163.com>
2016-04-14 15:48:43 +0800

    [+]redis缓存初始化

--- Tsy/Library/Cache/Cache.class.php
+++ Tsy/Library/Cache/Cache.class.php
@@ -11,5 +11,23 @@ namespace Tsy\Library\Cache;
 
 abstract class Cache
 {
-
+    public $handler;
+    public $options=[];
+    public function connect($type='',$options=array()) {
+        if(empty($type))  $type = C('DATA_CACHE_TYPE');
+        $class  =   strpos($type,'\\')? $type : 'Think\\Cache\\Driver\\'.ucwords(strtolower($type));
+        if(class_exists($class))
+            $cache = new $class($options);
+        else
+            E(L('_CACHE_TYPE_INVALID_').':'.$type);
+        return $cache;
+    }
+    //读取缓存
+    public function get($name){}
+    //写入缓存
+    public function set($name, $value, $expire = null) {}
+    //删除缓存
+    public function rm($name){}
+    //清除缓存
+    public function clear(){}
 }
--- TsyExample/a.php
+++ TsyExample/a.php
@@ -5,4 +5,8 @@
  * Date: 2016/4/12
  * Time: 23:31
  */
-echo 'a';
+//echo 'a';
+$Redis=new \Tsy\Library\Cache\Driver\Redis();
+$Redis->set('blibli','123456');
+$rs=$Redis->get('blibli');
+echo $rs;
