8256df3a6ad446b476a8915d73005d20bdc63dee
xinyustudio <castle@tansuyun.cn>
2016-04-15 19:59:40 +0800

    [+]HTTP协议封装

--- Tsy/Library/Swoole/Http.class.php
+++ Tsy/Library/Swoole/Http.class.php
@@ -14,6 +14,25 @@ use Tsy\Library\Swoole;
 class Http extends Swoole
 {
     function code($str){
+
+//GET / HTTP/1.1
+//Host: 127.0.0.1:60000
+//Connection: keep-alive
+//Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+//Upgrade-Insecure-Requests: 1
+//User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36
+//DNT: 1
+//Accept-Encoding: gzip, deflate, sdch
+//Accept-Language: zh-CN,zh;q=0.8
+
+//        A=1212&f=232
+        $_SERVER = array_merge($_SERVER,[
+            'REQUEST_TIME'=>time(),
+
+        ]);
+    }
+    function uncode($str)
+    {
         $HTTP = http_parse($str);
         if(is_array($HTTP)){
             if(isset($HTTP['Header'])){
@@ -36,24 +55,5 @@ class Http extends Swoole
             }
             $_REQUEST = array_merge($_GET,$_POST);
         }
-//GET / HTTP/1.1
-//Host: 127.0.0.1:60000
-//Connection: keep-alive
-//Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-//Upgrade-Insecure-Requests: 1
-//User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36
-//DNT: 1
-//Accept-Encoding: gzip, deflate, sdch
-//Accept-Language: zh-CN,zh;q=0.8
-
-//        A=1212&f=232
-        $_SERVER = array_merge($_SERVER,[
-            'REQUEST_TIME'=>time(),
-
-        ]);
-    }
-    function uncode($str)
-    {
-
     }
 }
