c5dd7427cf944b6f5ab176610cbcd41d6b7f3749
xinyustudio <castle@tansuyun.cn>
2016-04-18 09:28:02 +0800

    [+]Object对象自动获取table主表名称

--- Tsy/Library/Object.class.php
+++ Tsy/Library/Object.class.php
@@ -19,6 +19,14 @@ abstract class Object
     public $map=[
 //        自动生成
     ];//字段=》表名 映射
+    function __construct()
+    {
+        if(!$this->table){
+            $this->table = substr(__CLASS__,0,strlen(__CLASS__)-6);
+        }
+        //检测是否存在属性映射，如果存在则直接读取属性映射，没有则从数据库加载属性映射
+    }
+
     function add(){
 //        此处自动读取属性并判断是否是必填属性，如果是必填属性且无。。。则。。。
     }
