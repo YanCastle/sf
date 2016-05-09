e2af90aa415cf2d556c4b576eec81361565935d3
ghost <326956600@163.com>
2016-05-09 16:49:06 +0800

    [^] use get function can get objs

--- Tsy/Library/Controller.class.php
+++ Tsy/Library/Controller.class.php
@@ -53,13 +53,21 @@ class Controller
     }
     function get($ID=[]){
         if($ID){
-            return array_values(array_values(D($_GET['_c'])->obj($ID)))[0];
+            $ClassName=$_GET['_c'];
+            if(property_exists($this,$ClassName.'Object')){
+                $ObjectName=$ClassName.'Object';
+                $objs=$this->$ObjectName->get($ID);
+                return $objs;
+            }
         }
-        if(isset($this->Controller[$_GET['_c']])&&isset($this->Controller[$_GET['_c']][$_GET['_a']])){
-            return array_values(array_values(D($_GET['_c'])->obj([$_REQUEST[array_keys($this->Controller[$_GET['_c']][$_GET['_a']])[0]]])))[0];
-        }else{
-            return FALSE;
-        }
+//        if($ID){
+//            return array_values(array_values(D($_GET['_c'])->obj($ID)))[0];
+//        }
+//        if(isset($this->Controller[$_GET['_c']])&&isset($this->Controller[$_GET['_c']][$_GET['_a']])){
+//            return array_values(array_values(D($_GET['_c'])->obj([$_REQUEST[array_keys($this->Controller[$_GET['_c']][$_GET['_a']])[0]]])))[0];
+//        }else{
+//            return FALSE;
+//        }
     }
     function gets($P=1,$N=20,$Sort=[]){
         if($this->PRIKey){
