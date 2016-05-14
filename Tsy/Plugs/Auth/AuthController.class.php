0c081d20a464332c62ff79283de3685a65e8943c
鄢鹏权 <castle@tansuyun.cn>
2016-05-14 12:20:07 +0800

    [+]OAuth权限验证

---
+++ Tsy/Plugs/Auth/AuthController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: castle
+ * Date: 2016/5/14
+ * Time: 12:19
+ */
+
+namespace Tsy\Plugs\Auth;
+
+//权限管控控制器类，用于实现权限管控表的各种接口
+class AuthController
+{
+
+}
