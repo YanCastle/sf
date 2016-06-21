79473fbdb3a9fd1a76f2af389827a6810459664d
Castle <castle@tansuyun.cn>
2016-06-21 22:43:32 +0800

    [+]引入Smarty

---
+++ Tsy/Library/View.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: castle
+ * Date: 16-6-21
+ * Time: 下午10:40
+ */
+
+namespace Tsy\Library;
+
+
+class View
+{
+    function __construct($config)
+    {
+    }
+    function display(){}
+    function assign(){}
+}
