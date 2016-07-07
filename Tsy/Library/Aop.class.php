e879d47b5842f3b62449064fafc8fa1506fff1fb
鄢鹏权 <castle@tansuyun.cn>
2016-07-07 11:23:58 +0800

    [+]Aop的添加和执行

--- Tsy/Library/Aop.class.php
+++ Tsy/Library/Aop.class.php
@@ -12,7 +12,42 @@ namespace Tsy\Library;
 class Aop
 {
     protected static $config=[];
-    public static function add(string $name,callable $callback){}
-    public static function remove(string $name){}
-    public static function exec(string $name,array &$data){}
+    public static $AOP_BEFORE=0;
+    public static $AOP_AFTER=1;
+
+    /**
+     * 添加aop拦截
+     * @param string $name
+     * @param callable $callback
+     * @param $where
+     * @param int $order
+     * @return bool
+     */
+    public static function add(string $name,callable $callback,$where,$order=0){
+        if(!isset(self::$config[$name])){
+            self::$config[$name]=[[],[]];
+        }
+        if(!isset(self::$config[$name][$where][$order])){
+            self::$config[$name][$where][$order]=[];
+        }
+        self::$config[$name][$where][$order][]=$callback;
+        return true;
+    }
+    public static function remove(string $name,$where=-1){}
+
+    /**
+     * aop执行
+     * @param string $name
+     * @param $where
+     * @param array $data
+     */
+    public static function exec(string $name,$where,array &$data){
+        if(isset(self::$config[$name][$where])){
+            foreach (self::$config[$name][$where] as $callback){
+                if(is_callable($callback))
+                    call_user_func($callback,$data);
+            }
+        }
+    }
+    public static function regist(string $name,callable $callback,$where){}
 }
