19a833ec7c46651651806530e3940e19cbe153b1
Castle <castle@tansuyun.cn>
2016-10-31 15:18:11 +0800

    [#]查找账户是的错误修复

--- Tsy/Plugs/User/UserTrait.class.php
+++ Tsy/Plugs/User/UserTrait.class.php
@@ -93,7 +93,7 @@ trait UserTrait
      * @return array {'Email':"","Phone":"",'Account':"","UID":1}
      */
     function findAccount(string $Account){
-        return M($this->LoginView)->where(array_fill_keys(array_unique(array_merge($this->LoginAccountFields[$this->_map['Account']])),$Account))->getField('UID');
+        return M($this->LoginView)->where(array_fill_keys(array_unique(array_merge($this->LoginAccountFields,[$this->_map['Account']])),$Account))->getField('UID');
     }
 
     /**
