3f43b8016bcf6737f317880a9791758f0db27656
鄢鹏权 <castle@tansuyun.cn>
2016-11-06 23:00:58 +0800

    [+]添加UserObject对象。

---
+++ Tsy/Plugs/User/UserObject.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 鄢鹏权
+ * Date: 2016/11/6
+ * Time: 22:55
+ */
+
+namespace Tsy\Plugs\User;
+
+
+use Tsy\Library\Object;
+
+class UserObject extends Object
+{
+    use UserTrait;
+}
