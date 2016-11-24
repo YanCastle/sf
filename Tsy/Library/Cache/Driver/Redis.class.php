4fe1ed874113b858a3da2af906c1dc8363f6afe9
鄢鹏权 <castle@tansuyun.cn>
2016-11-24 19:15:03 +0800

    [#]是先判断Redis是否已经链接，如果已经连接则不再发起新的Redis链接。

--- Tsy/Library/Cache/Driver/Redis.class.php
+++ Tsy/Library/Cache/Driver/Redis.class.php
@@ -36,7 +36,9 @@ class Redis extends Cache implements CacheInterface
         $this->options['prefix'] =  isset($options['prefix'])?  $options['prefix']  :   C('DATA_CACHE_PREFIX');
         $this->options['length'] =  isset($options['length'])?  $options['length']  :   0;
         $func = $options['persistent'] ? 'pconnect' : 'connect';
-        $this->handler  = new \Redis();
+        if(!$this->handler){
+            $this->handler  = new \Redis();
+        }
         $options['timeout'] === false ?
             $this->handler->$func($options['host'], $options['port']) :
             $this->handler->$func($options['host'], $options['port'], $options['timeout']);
