ee8aafeba9af4acdb7084beac64a619450f5d25d
鄢鹏权 <castle@tansuyun.cn>
2016-12-07 18:21:42 +0800

    [#]变更Auth初始化sql的结构

--- Tsy/Library/Auth.class.php
+++ Tsy/Library/Auth.class.php
@@ -15,10 +15,10 @@ namespace Tsy\Library;
  * 1，是对规则进行认证，不是对节点进行认证。用户可以把节点当作规则名称实现对节点进行认证。
  *      $auth=new Auth();  $auth->check('规则名称','用户id')
  * 2，可以同时对多条规则进行认证，并设置多条规则的关系（or或者and）
- *      $auth=new Auth();  $auth->check('规则1,规则2','用户id','and') 
+ *      $auth=new Auth();  $auth->check('规则1,规则2','用户id','and')
  *      第三个参数为and时表示，用户需要同时具有规则1和规则2的权限。 当第三个参数为or时，表示用户值需要具备其中一个条件即可。默认为or
  * 3，一个用户可以属于多个用户组(think_auth_group_access表 定义了用户所属用户组)。我们需要设置每个用户组拥有哪些规则(think_auth_group 定义了用户组权限)
- * 
+ *
  * 4，支持规则表达式。
  *      在auth_rule 表中定义一条规则时，如果type为1， condition字段就可以定义规则表达式。 如定义{score}>5  and {score}<100  表示用户的分数在5-100之间时这条规则才会通过。
  */
@@ -100,7 +100,7 @@ class Auth{
      * @param  uid int     用户id
      * @return array       用户所属的用户组 array(
      *     array('uid'=>'用户id','group_id'=>'用户组id','title'=>'用户组名称','rules'=>'用户组拥有的规则id,多个,号隔开'),
-     *     ...)   
+     *     ...)
      */
     public function getGroups($uid) {
         static $groups = array();
@@ -118,7 +118,7 @@ class Auth{
     /**
      * 获得权限列表
      * @param integer $uid  用户id
-     * @param integer $type 
+     * @param integer $type
      */
     protected function getAuthList($uid,$type) {
         static $_authList = array(); //保存用户验证通过的权限列表
--- Tsy/Plugs/Auth/install.sql
+++ Tsy/Plugs/Auth/install.sql
@@ -1,39 +1,38 @@
 -- ----------------------------
--- {$PREFIX}auth_rule，规则表，
+-- prefix_auth_rule，规则表，
 -- ID:主键，Name：规则唯一标识, Title：规则中文名称 Status 状态：为1正常，为0禁用，condition：规则表达式，为空表示存在就验证，不为空表示按照条件验证
 -- ----------------------------
- DROP TABLE IF EXISTS `{$PREFIX}auth_rule`;
-CREATE TABLE `{$PREFIX}auth_rule` (
-    `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+ DROP TABLE IF EXISTS `prefix_auth_rule`;
+CREATE TABLE `prefix_auth_rule` (
+    `RID` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `Name` char(80) NOT NULL DEFAULT '',
     `Title` char(20) NOT NULL DEFAULT '',
     `Status` tinyint(1) NOT NULL DEFAULT '1',
     `Condition` char(100) NOT NULL DEFAULT '',
-    PRIMARY KEY (`ID`),
+    PRIMARY KEY (`RID`),
     UNIQUE KEY `Name` (`Name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 -- ----------------------------
--- {$PREFIX}auth_group 用户组表，
+-- prefix_auth_group 用户组表，
 -- ID：主键， Title:用户组中文名称， Rules：用户组拥有的规则ID， 多个规则","隔开，Status 状态：为1正常，为0禁用
 -- ----------------------------
- DROP TABLE IF EXISTS `{$PREFIX}auth_group`;
-CREATE TABLE `{$PREFIX}auth_group` (
-    `ID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+ DROP TABLE IF EXISTS `prefix_auth_group`;
+CREATE TABLE `prefix_auth_group` (
+    `GID` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `Title` char(100) NOT NULL DEFAULT '',
     `Status` tinyint(1) NOT NULL DEFAULT '1',
     `Rules` char(80) NOT NULL DEFAULT '',
-    PRIMARY KEY (`ID`)
+    PRIMARY KEY (`GID`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 -- ----------------------------
--- {$PREFIX}auth_group_access 用户组明细表
+-- prefix_auth_group_access 用户组明细表
 -- UID:用户ID，GroupID：用户组ID
 -- ----------------------------
-DROP TABLE IF EXISTS `{$PREFIX}auth_group_access`;
-CREATE TABLE `{$PREFIX}auth_group_access` (
+DROP TABLE IF EXISTS `prefix_auth_group_access`;
+CREATE TABLE `prefix_auth_group_access` (
     `UID` int(11) unsigned NOT NULL,
-    `GroupID` int(11) unsigned NOT NULL,
-    UNIQUE KEY `UIDGroupID` (`UID`,`GroupID`),
+    `GID` int(11) unsigned NOT NULL,
+    UNIQUE KEY `UIDGroupID` (`UID`,`GID`),
     KEY `UID` (`UID`),
-    KEY `GroupID` (`GroupID`)
+    KEY `GroupID` (`GID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
- */
