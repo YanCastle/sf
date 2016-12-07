d860fe05b8dd4a996a8c1710854a08fce0127ac9
鄢鹏权 <castle@tansuyun.cn>
2016-12-07 19:32:21 +0800

    [#]添加权限过滤的各种方法。

---
+++ Tsy/Plugs/User/build.sql
@@ -0,0 +1,109 @@
+drop table if exists prefix_user_access_dic;
+
+drop table if exists prefix_user_access_group_dic;
+
+drop table if exists prefix_user_group;
+
+drop table if exists prefix_user_group_access;
+
+drop table if exists prefix_user_group_dic;
+
+/*==============================================================*/
+/* Table: prefix_user_access_dic                                */
+/*==============================================================*/
+create table prefix_user_access_dic
+(
+   AID                  int unsigned not null auto_increment,
+   Title                char(250) not null,
+   Module               char(50) not null,
+   Class                char(50) not null,
+   Action               char(50) not null,
+   Type                 char(50) not null default 'Controller' comment 'Controller/Model/Object',
+   AGID                 char(50) not null
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+/*==============================================================*/
+/* Index: MCAT                                                  */
+/*==============================================================*/
+create unique index MCAT on prefix_user_access_dic
+(
+   Module,
+   Class,
+   Action,
+   Type
+);
+
+/*==============================================================*/
+/* Table: prefix_user_access_group_dic                          */
+/*==============================================================*/
+create table prefix_user_access_group_dic
+(
+   AGID                 int unsigned not null auto_increment,
+   Title                char(50) not null
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+/*==============================================================*/
+/* Table: prefix_user_group                                     */
+/*==============================================================*/
+create table prefix_user_group
+(
+   LID                  int unsigned not null auto_increment,
+   GID                  int unsigned not null,
+   UID                  int unsigned not null
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+/*==============================================================*/
+/* Index: UIDGID                                                */
+/*==============================================================*/
+create unique index UIDGID on prefix_user_group
+(
+   GID,
+   UID
+);
+
+/*==============================================================*/
+/* Table: prefix_user_group_access                              */
+/*==============================================================*/
+create table prefix_user_group_access
+(
+   LID                  int unsigned not null auto_increment,
+   GID                  int unsigned not null,
+   AID                  int unsigned not null,
+   `Condition`          varchar(1000) not null default ''
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+/*==============================================================*/
+/* Index: GIDAID                                                */
+/*==============================================================*/
+create index GIDAID on prefix_user_group_access
+(
+   AID,
+   GID
+);
+
+/*==============================================================*/
+/* Table: prefix_user_group_dic                                 */
+/*==============================================================*/
+create table prefix_user_group_dic
+(
+   GID                  int unsigned not null auto_increment,
+   Title                char(250) not null,
+   Sort                 int not null default 0,
+   PGID                 int unsigned not null default 0
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+/*==============================================================*/
+/* Index: Sort                                                  */
+/*==============================================================*/
+create index Sort on prefix_user_group_dic
+(
+   Sort
+);
+
+/*==============================================================*/
+/* Index: Title                                                 */
+/*==============================================================*/
+create index Title on prefix_user_group_dic
+(
+   Title
+);
