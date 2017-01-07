e122c184aecb57b44972364fda180098dc5a32bc
鄢鹏权 <castle@tansuyun.cn>
2017-01-07 21:41:36 +0800

    [#]自动加载composer的autoload

--- Tsy/Tsy.php
+++ Tsy/Tsy.php
@@ -5,6 +5,9 @@
  * Date: 2016/4/11
  * Time: 22:20
  */
+if(file_exists(dirname($APP_PATH).'/vendor/autoload.php')){
+    include dirname($APP_PATH).'/vendor/autoload.php';
+}
 include_once __DIR__.'/Tsy.class.php';
 $Tsy = new Tsy\Tsy();
 $Tsy->start();
