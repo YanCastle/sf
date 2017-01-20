85612d1246c6bfe4c4880e3b37549646af70d6d2
鄢鹏权 <castle@tansuyun.cn>
2017-01-20 14:28:53 +0800

    [#]add方法可以从data中获取数据

--- Tsy/Library/Functions/core.php
+++ Tsy/Library/Functions/core.php
@@ -291,6 +291,9 @@ function invokeClass($Class,$A,$data){
             }
             \Tsy\Library\Aop::exec('dispatch',\Tsy\Library\Aop::$AOP_BEFORE,$args);
             \Tsy\Library\Aop::exec('dispatch_'.get_class($Class).'::'.$A,\Tsy\Library\Aop::$AOP_BEFORE,$args);
+            if('add'===$A&&!$args['data']){
+                $args['data']=$_POST;
+            }
             $result = $ReflectMethod->invokeArgs($Class,$args);
             \Tsy\Library\Aop::exec('dispatch_'.get_class($Class).'::'.$A,\Tsy\Library\Aop::$AOP_AFTER,$result);
         }else{
