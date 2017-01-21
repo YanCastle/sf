c366c3d146cc65ba9a66b2a76c9cfdc8b489c56c
鄢鹏权 <castle@tansuyun.cn>
2017-01-21 16:36:37 +0800

    [#]缓存前缀定义为应用路径

--- Tsy/Config/config.php
+++ Tsy/Config/config.php
@@ -13,7 +13,7 @@ return [
     'DATA_CACHE_TIME'       =>  0,      // 数据缓存有效期 0表示永久缓存
     'DATA_CACHE_COMPRESS'   =>  false,   // 数据缓存是否压缩缓存
     'DATA_CACHE_CHECK'      =>  false,   // 数据缓存是否校验缓存
-    'DATA_CACHE_PREFIX'     =>  '',     // 缓存前缀
+    'DATA_CACHE_PREFIX'     =>  md5(APP_PATH),     // 缓存前缀
     'DATA_CACHE_TYPE'       =>  'File',  // 数据缓存类型,支持:File|Db|Apc|Memcache|Shmop|Sqlite|Xcache|Apachenote|Eaccelerator
     'DATA_CACHE_TEMP_TYPE'  =>  'File',  // 数据缓存类型,支持:File|Db|Apc|Memcache|Shmop|Sqlite|Xcache|Apachenote|Eaccelerator
     'DATA_CACHE_PATH'       =>  TEMP_PATH,// 缓存路径设置 (仅对File方式缓存有效)
