c5a4adbb092ed5dfcbbc3f4e95b7f9d9ff232771
鄢鹏权 <castle@tansuyun.cn>
2017-02-19 18:21:02 +0800

    [+]添加CURL定义和APP_DEBUG定义检测，当检测到都存在时则进入CURL命令调试模式。

--- Tsy/Tsy.php
+++ Tsy/Tsy.php
@@ -5,8 +5,18 @@
  * Date: 2016/4/11
  * Time: 22:20
  */
-if('options'==strtolower($_SERVER['REQUEST_METHOD'])){
+if(isset($_SERVER['REQUEST_METHOD'])&&'options'==strtolower($_SERVER['REQUEST_METHOD'])){
     exit();
+}
+if(defined('CURL')&&defined('APP_DEBUG')&&APP_DEBUG&&CURL){
+    $cmd = explode(' ',CURL);
+    $url = $cmd[1];
+    $query  = parse_url(trim($url,'\''),PHP_URL_QUERY);
+    parse_str($query,$GET);
+    $_GET = array_merge($_GET,$GET);
+    parse_str(trim($cmd[count($cmd)-2],'\''),$POST);
+    $_POST = array_merge($_POST,$POST);
+    unset($GET,$POST);
 }
 if(file_exists(dirname($APP_PATH).'/vendor/autoload.php')){
     include dirname($APP_PATH).'/vendor/autoload.php';
