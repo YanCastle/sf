3050ec2e04043acc2a4a779e0261c358e608c17a
鄢鹏权 <castle@tansuyun.cn>
2017-02-19 09:19:03 +0800

    [#]所有options请求直接通过

--- Tsy/Tsy.php
+++ Tsy/Tsy.php
@@ -5,6 +5,9 @@
  * Date: 2016/4/11
  * Time: 22:20
  */
+if('options'==strtolower($_SERVER['REQUEST_METHOD'])){
+    exit();
+}
 if(file_exists(dirname($APP_PATH).'/vendor/autoload.php')){
     include dirname($APP_PATH).'/vendor/autoload.php';
 }
