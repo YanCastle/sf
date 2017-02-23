6eb062f0ebb3b3478709dc3d78d96cca07a8009a
鄢鹏权 <castle@tansuyun.cn>
2017-02-23 10:14:08 +0800

    [+]默认数据库配置信息设定

--- Tsy/Config/config.php
+++ Tsy/Config/config.php
@@ -84,5 +84,12 @@ return [
         'DISPATCH'=>["\\Tsy\\Tsy::\$Mode",'dispatch'],
         'OUT'=>'\\Tsy\\Tsy::$Mode::output',
     ],
-    'SQL_PREFIX'=>['{$PREFIX}','prefix_']
+    'SQL_PREFIX'=>['{$PREFIX}','prefix_'],
+    'DB_TYPE'               =>  'mysqli',     // 数据库类型
+    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
+    'DB_NAME'               =>  'test',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  '',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  '',    // 数据库表前缀
 ];
