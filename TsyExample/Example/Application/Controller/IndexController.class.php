2f2000fd7e2a571edb950961db95cbf8d190d611
鄢鹏权 <castle@tansuyun.cn>
2017-02-26 13:55:11 +0800

    [^]Example相关

--- TsyExample/Example/Application/Controller/IndexController.class.php
+++ TsyExample/Example/Application/Controller/IndexController.class.php
@@ -8,13 +8,40 @@
 
 namespace Application\Controller;
 use Tsy\Library\Controller;
+use Tsy\Library\Storage;
 use Tsy\Plugs\File\File;
 
 class IndexController extends Controller
 {
     function index(){
-        echo C('FILE_UPLOAD_TYPE'),"\r\n";
-        var_dump(File::upload());
+        if(!file_exists('config.json')){
+            return 'config.json配置文件不存在';
+        }
+        $Config = file_get_contents('config.json');
+        if($Config){
+            $Config = json_decode($Config,true);
+        }
+        if(
+            isset($Config['UCLOUD_PUBLIC_KEY'])&&$Config['UCLOUD_PUBLIC_KEY']&&
+            isset($Config['UCLOUD_PRIVATE_KEY'])&&$Config['UCLOUD_PRIVATE_KEY']&&
+            isset($Config['UCLOUD_BUKKET'])&&$Config['UCLOUD_BUKKET']&&
+            isset($Config['SYNC_DIR'])&&$Config['SYNC_DIR']&&
+            isset($Config['UCLOUD_PROXY_SUFFIX'])&&$Config['UCLOUD_PROXY_SUFFIX']
+        ){
+            C($Config);
+        }else{
+            return '错误的配置文件';
+        }
+        Storage::connect('UCloud');
+        $SyncDir = C('SYNC_DIR');
+        if(is_dir($SyncDir)){
+            each_dir($SyncDir,null,function($path)use($SyncDir){
+                $rs = Storage::put(str_replace([$SyncDir,"\\"],['','/'],$path),file_get_contents($path));
+            });
+            return '成功';
+        }else{
+            return '目录不存在;';
+        }
     }
     /**
      * 空操作
